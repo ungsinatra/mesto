7acb29cf0e51b13bc5c35d7206701acb14ca6405
ungsintra <mamirov.21@mail.ru>
2022-10-08 16:42:20 +0300

    refactoring class formValidator

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -2,9 +2,11 @@ class FormValidator {
   constructor(selectors, form) {
     this._selectors = selectors;
     this._form = form;
+    this._inputLists = Array.from(this._form.querySelectorAll(this._selectors.popupInput));
+    this._saveButton = this._form.querySelector('.popup__btn-save');
   }
   enableValidation() {
-    this._setListenerInputs(this._form);
+    this._setListenerInputs();
   }
   _hideError(formInput) {
     const errorElement = this._form.querySelector(`.${formInput.id}-error`);
@@ -19,18 +21,18 @@ class FormValidator {
     errorElement.classList.add(this._selectors.popupInputError);
     return errorElement;
   }
-  _hasInvalidInputs(inputLists) {
-    return inputLists.some((input) => {
+  _hasInvalidInputs() {
+    return this._inputLists.some((input) => {
       return !input.validity.valid;
     });
   }
-  _toggleSubmitBtn(inputList, btn) {
-    if (this._hasInvalidInputs(inputList)) {
-      btn.setAttribute("disabled", true);
-      btn.classList.add(this._selectors.popupInactiveBtn);
+  _toggleSubmitBtn() {
+    if (this._hasInvalidInputs()) {
+      this._saveButton.setAttribute("disabled", true);
+      this._saveButton.classList.add(this._selectors.popupInactiveBtn);
     } else {
-      btn.removeAttribute("disabled");
-      btn.classList.remove(this._selectors.popupInactiveBtn);
+      this._saveButton.removeAttribute("disabled");
+      this._saveButton.classList.remove(this._selectors.popupInactiveBtn);
     }
   }
   _validationForms(formInput) {
@@ -40,18 +42,12 @@ class FormValidator {
       this._hideError(formInput);
     }
   }
-  _setListenerInputs(formElement) {
-    const submitButton = formElement.getElementsByTagName(
-      this._selectors.button
-    )[0];
-    const inputList = Array.from(
-      formElement.querySelectorAll(this._selectors.popupInput)
-    );
-    this._toggleSubmitBtn(inputList, submitButton);
-    inputList.forEach((input) => {
+  _setListenerInputs() {
+    this._toggleSubmitBtn();
+    this._inputLists.forEach((input) => {
       input.addEventListener("input", () => {
         this._validationForms(input);
-        this._toggleSubmitBtn(inputList, submitButton);
+        this._toggleSubmitBtn();
       });
     });
   }
