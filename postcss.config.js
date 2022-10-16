e36fa5e9a4fab08fdcbeab6eea27a76bd2be4094
ungsintra <mamirov.21@mail.ru>
2022-10-16 19:15:05 +0300

    webpack build 1.0.2

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,17 @@
+
+
+// подключите плагины в файл
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 
 module.exports = {
-    plagins:[
-        // подключите autoprefixer
-        autoprefixer,
-        // cssnano при подключении нужно передать объект опций
-        // { preset: default } говорит о том, что нужно использовать
-        // стандартные настройки минификации
-        cssnano({ preset: 'default' })
-      ]
-}
+  // подключите плагины к PostCSS
+  plugins: [
+    // подключите autoprefixer
+    autoprefixer,
+    // cssnano при подключении нужно передать объект опций
+    // { preset: default } говорит о том, что нужно использовать
+    // стандартные настройки минификации
+    cssnano({ preset: 'default' })
+  ]
+};
