29629d6c6e6b0df63ec1165aa3d12f5f2040544e
ungsintra <mamirov.21@mail.ru>
2022-10-16 21:20:59 +0300

    fix fiilinputs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-import './index.css';
-import {FormValidator}  from "../components/FormValidator.js";
-import {Card} from "../components/Card.js";
-import {Section} from "../components/Section.js"
+import "./index.css";
+import { FormValidator } from "../components/FormValidator.js";
+import { Card } from "../components/Card.js";
+import { Section } from "../components/Section.js";
 import { Popup } from "../components/Popup.js";
 import { PopupWithImage } from "../components/PopupWithImage.js";
 import { PopupWithForm } from "../components/PopupWithForm.js";
@@ -20,103 +20,89 @@ import {
   forms,
   popupImage,
   popupHeading,
-  profile,
   profileEditBtn,
   addCardBtn,
   name,
   work,
-  template,
   placeContainer,
-} from '../utils/constants.js'
+} from "../utils/constants.js";
 
-
-
-
-// CARD ADD
-const cardList = new Section({items:initialCards,renderer:item => {
-  const card  = new Card(item.name,item.link,cardSelectors,template,() => {
-    popupImgHandler(item);
-
-  });
-  const cardtemplate = card.genaraeteTemplate();
-  cardList.addItem(cardtemplate);
-}},placeContainer);
+fillProfile({ name: name.textContent, desc: work.textContent });
+// CARD ADD inition cards
+const cardList = new Section(
+  {
+    items: initialCards,
+    renderer: (item) => {
+      const card = new Card(item.name, item.link, cardSelectors, () => {
+        popupImgHandler(item);
+      });
+      const cardtemplate = card.genaraeteTemplate();
+      cardList.addItem(cardtemplate);
+    },
+  },
+  placeContainer
+);
 cardList.renderItems();
 
-function popupImgHandler({name,link}){
+function popupImgHandler({ name, link }) {
   const popupImg = new PopupWithImage(popupWithImg);
-  popupImage.src = link; 
-  popupHeading.textContent = name; 
-  popupWithImg.classList.add(selectors.popupOpen); 
+  popupImage.src = link;
+  popupHeading.textContent = name;
+  popupWithImg.classList.add(selectors.popupOpen);
   popupImg.setEventListeners();
 }
 
 // Add card form Input
 const popupCards = new PopupWithForm(popupCard, (FormValues) => {
   const arrFromValues = [FormValues];
-  const cardList = new Section({items:arrFromValues,renderer:item => {
-    const card  = new Card(item.name,item.link,cardSelectors,template,() => {
-      popupImgHandler(item);
-  
-    });
-    const cardtemplate = card.genaraeteTemplate();
-    cardList.addItem(cardtemplate);
-  }},placeContainer);
+  const cardList = new Section(
+    {
+      items: arrFromValues,
+      renderer: (item) => {
+        const card = new Card(item.name, item.link, cardSelectors, () => {
+          popupImgHandler(item);
+        });
+        const cardtemplate = card.genaraeteTemplate();
+        cardList.addItem(cardtemplate);
+      },
+    },
+    placeContainer
+  );
   cardList.renderItems();
-  popupCards.close();
-})
-popupCards.setEventListeners()
-
+  popupCards.close(true);
+});
+popupCards.setEventListeners();
 
 // add Listerner for popup open buttons
 
-addCardBtn.addEventListener('click',(evt) => {
+addCardBtn.addEventListener("click", () => {
   const openCardPopup = new Popup(popupCard);
-  openCardPopup.open()
-})
+  openCardPopup.open();
+});
 
 // add Profile lsitener and set profile info
-fillProfile({name:name.textContent,desc:work.textContent})
-const profileSet = new PopupWithForm(popupProfile,(profileValues) => { 
-  const profile = new UserInfo({name:name,desc:work});
+const profileSet = new PopupWithForm(popupProfile, (profileValues) => {
+  const profile = new UserInfo({ name: name, desc: work });
   profile.setUserInfo(profileValues);
   const profileInfo = profile.getUserInfo();
-  profileSet.close();
-
+  profileSet.close(false);
 });
 profileSet.setEventListeners();
 
-
-profileEditBtn.addEventListener('click',(evt) => {
+profileEditBtn.addEventListener("click", (evt) => {
   evt.preventDefault();
   const popup = new Popup(popupProfile);
   popup.open();
-})
+});
 
-function fillProfile({name,desc}){
+function fillProfile({ name, desc }) {
   nameInput.value = name;
   descInput.value = desc;
 }
 
-
-
-
-
-
 // Add Validation on forms
-forms.forEach(form => {
-    const validForm = new FormValidator(popupSelecors,form);
-    validForm.enableValidation()
-})
-
-fillProfile({name:name.textContent,desc:work.textContent});
-
-
-
-
-
-
-
-
-
-
+forms.forEach((form) => {
+  const validForm = new FormValidator(popupSelecors, form);
+  validForm.enableValidation();
+});
+fillProfile({ name: name.textContent, desc: work.textContent });
